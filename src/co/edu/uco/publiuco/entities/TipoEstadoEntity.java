227991b7ee19909cb0b8b027164a6abd743bab8b
Carlos Tejada <carlos.tejada5620@soyuco.edu.co>
2023-05-19 18:58:15 -0500

    Clase DOO 0519

--- src/co/edu/uco/publiuco/entities/TipoEstadoEntity.java
+++ src/co/edu/uco/publiuco/entities/TipoEstadoEntity.java
@@ -21,6 +21,20 @@ public class TipoEstadoEntity {
         setNombre(nombre);
         setDescripcion(descripcion);
     }
+    
+    
+    
+    public static final TipoEstadoEntity createWithIdentificador(final UUID identificador) {
+    	return new TipoEstadoEntity(identificador, UtilText.getDefaultValue(), UtilText.getDefaultValue());
+    }
+    
+    public static final TipoEstadoEntity createWithNombre(final String nombre) {
+    	return new TipoEstadoEntity(UtilUUID.getDefaultValue(), nombre, UtilText.getDefaultValue());
+    }
+    
+    public static final TipoEstadoEntity createWithDescripcion(final String descripcion) {
+    	return new TipoEstadoEntity(UtilUUID.getDefaultValue(), UtilText.getDefaultValue(), descripcion);
+    }
 
     public UUID getIdentificador() {
         return identificador;
@@ -35,8 +49,9 @@ public class TipoEstadoEntity {
     }
 
 
-    private void setIdentificador(final UUID identificador) {
+    private TipoEstadoEntity setIdentificador(final UUID identificador) {
         this.identificador = UtilUUID.getDefault(identificador);
+        return this;
     }
 
     private void setNombre(final String nombre) {
